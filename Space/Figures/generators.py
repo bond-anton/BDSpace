b8878f694c3450f69a2839100f0889a6271f9bce
Anton Bondarenko <bond.anton@gmail.com>
2016-03-02 00:40:14 +0300

    Sphere generator added

--- Space/Figures/generators.py
+++ Space/Figures/generators.py
@@ -8,40 +8,30 @@ def generate_cuboid(a=1, b=1, c=1, origin=np.array([0, 0, 0])):
     return cube[:] - origin, dims
 
 
-def generate_sphere(r, theta, phi):
+def generate_sphere(phi, theta, r):
     """
     Generate points for structured grid for a spherical shell volume.
     This method is useful for generating a unstructured cylindrical mesh for VTK.
-    :param r: radius grid
-    :param phi: azimuthal angle grid
-    :param theta: polar angle grid
+    :param phi: azimuthal angle array
+    :param theta: polar angle array
+    :param r: radius of sphere
+    :param hole_r: array of radius values for filled sphere
     """
-    # Find the x values and y values for each plane.
-    x_plane = (np.sin(theta) * np.cos(phi) * r[:, None]).ravel()
-    y_plane = (np.sin(theta) * np.cos(phi) * r[:, None]).ravel()
-    z = (np.cos(theta) * r[:, None])
-
-    # Allocate an array for all the points.  We'll have len(x_plane)
-    # points on each plane, and we have a plane for each z value, so
-    # we need len(x_plane)*len(z) points.
-    points = np.empty([len(x_plane) * len(z), 3])
-
-    # Loop through the points for each plane and fill them with the
-    # correct x,y,z values.
+    #z = r * np.cos(theta)
+    points = np.empty([len(phi) * len(r) * len(theta), 3])
     start = 0
-    for z_plane in z:
-        end = start+len(x_plane)
-        # slice out a plane of the output points and fill it
-        # with the x,y, and z values for this plane.  The x,y
-        # values are the same for every plane.  The z value
-        # is set to the current z
+    for th in theta:
+        x_plane = (np.cos(phi) * r[:, None] * np.sin(th)).ravel()
+        y_plane = (np.sin(phi) * r[:, None] * np.sin(th)).ravel()
+        z_plane = (r[:, None] * np.cos(th) * (np.cos(phi)**2 + np.sin(phi)**2)).ravel()
+        end = start + len(x_plane)
         plane_points = points[start:end]
         plane_points[:, 0] = x_plane
         plane_points[:, 1] = y_plane
         plane_points[:, 2] = z_plane
         start = end
-
-    return points
+    dims = (len(phi), len(r), len(theta))
+    return points, dims
 
 
 def generate_cone(phi, z=np.array([0, 100.0]), theta=np.pi/4, hole_radius=5, r_min=20):
