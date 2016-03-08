e7b4d038db0b8ce92c3f96a3036b91e9169a4135
Anton Bondarenko <bond.anton@gmail.com>
2016-03-09 00:00:05 +0300

    Added volume calculation for Figures.

--- Space/Figure/__init__.py
+++ Space/Figure/__init__.py
@@ -8,3 +8,18 @@ class Figure(Space):
 
     def __str__(self):
         return 'Figure: ' + self.name
+
+    def volume(self):
+        """
+        Calculates volume of the Figure.
+        :return: Volume (float)
+        """
+        return 0.0
+
+    def external_volume(self):
+        """
+        calculates volume of the outer shell of the Figure.
+        For figures without voids and inner cavities returns the same value as volume function.
+        :return: Volume float
+        """
+        return self.volume()
