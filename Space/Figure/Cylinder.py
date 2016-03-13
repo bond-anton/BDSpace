6b0642591933db796fc4001ed6a1b6a22be4ad45
Anton Bondarenko <bond.anton@gmail.com>
2016-03-13 23:12:36 +0300

    Refined cylindrical shapes surface area and volume calculations

--- Space/Figure/Cylinder.py
+++ Space/Figure/Cylinder.py
@@ -1,6 +1,7 @@
 from __future__ import division
 import numpy as np
 
+from Space.Coordinates.transforms import reduce_angle
 from Space.Figure import Figure
 
 
@@ -10,9 +11,32 @@ class CylindricalWedge(Figure):
                  r_inner=0.0, r_outer=1.0, phi=np.pi/2, z=np.array([0.0, 1.0])):
         self.r_inner = r_inner
         self.r_outer = r_outer
-        self.phi = phi
+        self.phi = reduce_angle(float(phi))
         self.z = np.array(z, dtype=float)
         super(CylindricalWedge, self).__init__(name, coordinate_system=coordinate_system)
+
+    def inner_volume(self):
+        return 0.0
+
+    def external_volume(self):
+        h = abs(max(self.z) - min(self.z))
+        v_inner = h * self.r_inner**2 * self.phi / 2
+        v_outer = h * self.r_outer**2 * self.phi / 2
+        return v_outer - v_inner
+
+    def inner_surface_area(self):
+        return 0.0
+
+    def external_surface_area(self):
+        h = abs(max(self.z) - min(self.z))
+        s_outer = h * self.r_outer * self.phi
+        s_inner = h * self.r_inner * self.phi
+        s_bases = (self.r_outer**2 - self.r_inner**2) * self.phi / 2
+        if self.phi == 2 * np.pi:
+            s_cut = 0
+        else:
+            s_cut = 2 * h * (self.r_outer - self.r_inner)
+        return s_outer + s_inner + s_bases + s_cut
 
 
 class Cylinder(CylindricalWedge):
