9491f7bc4ce0209adb7705eb54f02734a2a7be4d
bond_anton <bond.anton@gmail.com>
2021-01-17 22:41:33 +0300

    Version 0.3.13 corrected import in __init__.py

--- BDSpace/__init__.py
+++ BDSpace/__init__.py
@@ -1,8 +1,8 @@
 from .Space import Space
-import Coordinates
-import Curve
-import Field
-import Figure
-import Pathfinder
+# import Coordinates
+# import Curve
+# import Field
+# import Figure
+# import Pathfinder
 
-__all__ = ['Space', 'Coordinates', 'Curve', 'Field', 'Figure', 'Pathfinder']
+# __all__ = ['Space', 'Coordinates', 'Curve', 'Field', 'Figure', 'Pathfinder']
